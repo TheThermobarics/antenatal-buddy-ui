1510bb2f4477b46c90e792423a319147f906ca40
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-09-22 18:53:27 +0530

    initial setup

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Hero from "./../components/Hero/Hero";
+import Features from "./../components/Features/Features";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
+};
+
+export default LandingPage;
